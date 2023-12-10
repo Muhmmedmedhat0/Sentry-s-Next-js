f77c9331a6915d3315e1d45a16f552dba0abbb02
muhmmed_medhat <muhmmedmedhat0@gmail.com>
2023-12-10 11:03:48 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useState } from 'react';
+
+export default function Home() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleIncrement = () => {
+    if (count === 5) {
+      throw new Error('Count reached 5');
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const resetCount = () => setCount(0);
+
+  return (
+    <main>
+      <h1>Hello world</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>click me</button>
+      <button onClick={resetCount}>reset</button>
+    </main>
+  );
+}
